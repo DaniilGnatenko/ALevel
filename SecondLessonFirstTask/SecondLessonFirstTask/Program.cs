ad616639734c58f4317c504121b68d3d13c41bb9
Daniil <dnlgntenko@gmail.com>
2023-10-13 21:26:25 +0300

    my first commit

---
+++ SecondLessonFirstTask/SecondLessonFirstTask/Program.cs
@@ -0,0 +1,90 @@
+﻿
+
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+Console.WriteLine("Hello! You can sort there random values in array!");
+Console.WriteLine("Enter minimum value in array:");
+var minValueAsString = Console.ReadLine();
+int minValue;
+while (!int.TryParse(minValueAsString, out minValue))
+    {
+    Console.WriteLine("This is not a number! Enter minimum Value:");
+    minValueAsString = Console.ReadLine();
+    }
+
+
+
+Console.WriteLine("Enter maximum value in array:");
+var maxValueAsString = Console.ReadLine();
+int maxValue;
+while (!int.TryParse(maxValueAsString, out maxValue))
+{
+    Console.WriteLine("This is not a number! Enter maximum Value:");
+    maxValueAsString = Console.ReadLine();
+}
+while (maxValue == minValue)
+{
+    Console.WriteLine("Maximum value can't be the same as minimum value!" + "\nPlease, Enter new Maximum Value:");
+    maxValueAsString = Console.ReadLine();
+    while (!int.TryParse(maxValueAsString, out maxValue))
+    {
+        Console.WriteLine("This is not a number! Enter maximum Value:");
+        maxValueAsString = Console.ReadLine();
+    }
+}
+while (maxValue < minValue)
+{
+    Console.WriteLine("Maximum value can't be less then minimum value!" + "\nPlease, Enter new Maximum Value:");
+    maxValueAsString = Console.ReadLine();
+    while (!int.TryParse(maxValueAsString, out maxValue))
+    {
+        Console.WriteLine("This is not a number! Enter maximum Value:");
+        maxValueAsString = Console.ReadLine();
+    }
+}
+
+
+Console.WriteLine("Enter length of array:");
+var ArrayLenghtAsString = Console.ReadLine();
+int ArrayLenght;
+while (!int.TryParse(ArrayLenghtAsString, out ArrayLenght))
+{
+    Console.WriteLine("This is not a number! Enter array lenght:");
+    ArrayLenghtAsString = Console.ReadLine();
+}
+
+
+int[] array = new int[ArrayLenght];
+
+
+        for (int i = 0; i < array.Length; i++)
+        {
+        int number = i + 1;
+        array[i] = new Random().Next(minValue, maxValue);
+        Console.WriteLine(number + ": " + array[i]);
+        }
+
+Console.WriteLine("To show values between -100 and 100 type Show" +
+    "\nIf you want exit - type Exit");
+string choise = Console.ReadLine().ToLower();
+switch (choise)
+{
+    case "exit":
+        Console.Clear();
+        break;
+    case "show":
+        Console.WriteLine("This values in array between -100 and 100:");
+        for (int i = 0;i < array.Length;i++)
+        {
+            int number = i + 1;
+            if (array[i] > -100 && array[i] < 100)
+            {
+                Console.WriteLine(number + ": " + array[i]);
+            }
+        }
+       break;
+
+}
+
+
+Console.ReadKey();
---
+++ SecondLessonSecondTask/SecondLessonSecondTask/Program.cs
@@ -0,0 +1,42 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+Console.WriteLine("Hello! This is sorting app for array values!" + "\nThis is random values in First Array:");
+int[] arrayA = new int[20];
+int[] arrayB = new int[20];
+for (int i = 0; i < arrayA.Length; i++)
+{
+    int number = i + 1;
+    arrayA[i] = new Random().Next(-5000, 5000);
+    Console.WriteLine(number + ": " + arrayA[i]);
+    if (arrayA[i] <= 888)
+    {
+        arrayB[i] = arrayA[i];
+    }
+    else
+    {
+        arrayB[i] = new Random().Next(-5000, 5000);
+    }
+}
+
+Console.WriteLine("Press enter to continue...");
+Console.ReadKey();
+
+Console.WriteLine("This is values in Second array, they include values <=888 from First Array");
+for (int i = 0;i < arrayB.Length;i++)
+{
+    int number = i + 1;
+    Console.WriteLine(number + ": " + arrayB[i]);
+}
+
+Console.WriteLine("Now i will sort Second Array in descending order");
+Console.WriteLine("Press enter to continue...");
+Console.ReadKey();
+Array.Sort(arrayA);
+Array.Sort(arrayB);
+Array.Reverse(arrayB);
+Console.WriteLine("This is First Array:");
+Console.WriteLine("({0})", string.Join(",", arrayA));
+Console.WriteLine("This is Second Array:");
+Console.WriteLine("({0})", string.Join(",", arrayB));
+Console.ReadKey();
+
