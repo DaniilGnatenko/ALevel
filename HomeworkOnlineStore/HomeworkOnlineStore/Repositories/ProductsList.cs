740e5ee80f8ec94b8d0a7febfadb3ee78243dd60
Daniil <dnlgntenko@gmail.com>
2023-11-01 11:50:00 +0200

    Homework 2 module 2 lesson

---
+++ HomeworkOnlineStore/HomeworkOnlineStore/Repositories/ProductsList.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HomeworkOnlineStore.Models;
+
+namespace HomeworkOnlineStore.Repositories
+{
+    internal class ProductsList
+    {
+        public Product[] productsList = new Product[10];
+        private readonly string[] productNames = new string[10] { "bread", "milk", "ice cream",
+            "beer", "eggs", "ham",
+            "Coca Cola", "cucumber", "tomato",
+            "Snikers" };
+        private int[] _amountInStore = new int[10];
+        private static ProductsList instance;
+
+        public int[] AmountInStore {  get { return _amountInStore; } }
+        private ProductsList()
+        {
+            for (int i = 0; i < productsList.Length; i++)
+            {
+                productsList[i] = new Product();
+                _amountInStore[i] = new Random().Next(3,16);
+            }
+        }
+
+        public static ProductsList GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new ProductsList();
+                }
+                return instance;
+            }
+        }
+
+        public void ShowProductsList()
+        {
+            for (int i = 0; i < productsList.Length; i++)
+            {
+                productsList[i].ProductID = i + 1;
+                productsList[i].Name = productNames[i];
+                
+                Console.WriteLine("ProductID: {0} Product: {1}, price: {2:N2}, amount {3}", productsList[i].ProductID, productsList[i].Name, productsList[i].Price, _amountInStore[i]);
+            }
+        }
+
+        public void TakeProductFromList(int productIndex)
+        {
+            if (_amountInStore[productIndex - 1] > 0)
+            {
+                _amountInStore[productIndex - 1] -= 1;
+            }
+            else
+            {
+                Console.WriteLine("This product is out of stock.");
+            }
+        }
+
+        public void ReturnProductToList(int productIndex)
+        {
+            _amountInStore[productIndex - 1] += 1;
+        }
+    }
+}
---
+++ HomeworkOnlineStore/HomeworkOnlineStore/Repositories/ShoppingCart.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HomeworkOnlineStore.Models;
+
+namespace HomeworkOnlineStore.Repositories
+{
+    internal class ShoppingCart
+    {
+        private ProductsList productList = ProductsList.GetInstance;
+        private static ShoppingCart instance;
+        private ShoppingCart()
+        {
+            
+        }
+
+        public static ShoppingCart GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new ShoppingCart();
+                }
+                return instance;
+            }
+        }
+        public List<Product> productsInCart = new List<Product>();
+        public List<int> amountProductInCart = new List<int>();
+
+        public void AddToCart(int productID)
+        {
+            
+            if (!productsInCart.Contains(productList.productsList[productID - 1]))
+            {
+                productsInCart.Add(productList.productsList[productID - 1]);
+                amountProductInCart.Add(1);
+                Console.WriteLine("You added {0}", productList.productsList[productID - 1].Name);
+                
+            }
+            else if (productsInCart.Contains(productList.productsList[productID - 1]))
+            {
+                int productIndex = productsInCart.IndexOf(productList.productsList[productID - 1]);
+                amountProductInCart.Insert(productIndex, amountProductInCart[productIndex] + 1);
+                amountProductInCart.RemoveAt(productIndex + 1);
+                Console.WriteLine("You added {0}", productList.productsList[productID - 1].Name);
+            }
+        }
+
+        public void RemoveFromCart(int productID)
+        {
+            int productIndex = productsInCart.IndexOf(productList.productsList[productID - 1]);
+            if (productsInCart.Contains(productList.productsList[productID - 1]) && amountProductInCart[productIndex] > 1)
+            {
+                amountProductInCart.Insert(productIndex, amountProductInCart[productIndex] - 1);
+                amountProductInCart.RemoveAt(productIndex + 1);
+                Console.WriteLine("You removed {0}", productList.productsList[productID - 1].Name);
+            }
+            else if (productsInCart.Contains(productList.productsList[productID - 1]) && amountProductInCart[productIndex] == 1)
+            {
+                productsInCart.RemoveAt(productIndex);
+                amountProductInCart.RemoveAt(productIndex);
+                Console.WriteLine("You removed {0}", productList.productsList[productID - 1].Name);
+            }
+            else if (!productsInCart.Contains(productList.productsList[productID - 1]))
+            {
+                
+                Console.WriteLine("You don't have this in your cart!");
+            }
+        }
+        public void ShowCart()
+        {
+            for (int i = 0;  i < productsInCart.Count; i++)
+            {
+                Console.WriteLine("Products in your cart: {0} amount {1} price per one {2:N2}", productsInCart[i].Name, amountProductInCart[i], productsInCart[i].Price);
+                
+            }
+        }
+    }
+}
+
+
