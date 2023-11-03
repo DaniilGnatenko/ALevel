84b2d23cdc72a4238de9a013312237738237e438
Daniil <dnlgntenko@gmail.com>
2023-11-03 16:23:16 +0200

    added Homework 2.3

---
+++ NewYearGift/NewYearGift/Models/Sweet.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewYearGift.Models
+{
+    interface IShowSweet
+    {
+        void ShowSweet()
+        {
+            
+        }
+    }
+   
+    internal class Sweet : IShowSweet
+    {
+        public int length;
+        public int height;
+        public int width;
+        public int volume;
+        public double density;
+        public double mass;
+        public string[] names = new string[10] { "Snikers", "Mars", "Bounty", "Roshen", "AVK", "Konti", "Milky Way", "Twix", "Milka", "Nestle" };
+        public string name;
+        
+        public Sweet()
+        {
+            length = new Random().Next(1, 8);
+            height = new Random().Next(1, 6);
+            width = new Random().Next(1, 4);
+            volume = length * height * width;
+            density = new Random().NextDouble();
+            mass = density * volume;
+            name = names[new Random().Next(0, 10)];
+        }
+
+        public void ShowSweet()
+        {
+            Console.WriteLine("This is {0} sweet", name);
+        }
+
+    }
+    internal class Candy : Sweet, IShowSweet 
+    {
+        private string[] _wrapperTypes = new string[3] { "paper", "plastic", "without" };
+        public string wrapper;
+
+        public Candy()
+        {
+            wrapper = _wrapperTypes[new Random().Next(0, 3)];
+        }
+        public void ShowSweet()
+        {
+            Console.WriteLine("This is candy {0} in {1} wrapper", name, wrapper);
+        }
+    }
+    internal class ChocolateBar : Sweet, IShowSweet
+    {
+        private string[] _chocolateTypes = new string[3] { "milk", "dessert", "bitter" };
+        public string chocolateType;
+        public ChocolateBar()
+        {
+            chocolateType = _chocolateTypes[new Random().Next(0, 3)];
+        }
+        public void ShowSweet()
+        {
+            Console.WriteLine("This is chocolate bar {0} with {1} chocolate", name, chocolateType);
+        }
+    }
+    internal class Cookie : Sweet, IShowSweet
+    {
+        private string[] _chocolateTypes = new string[3] { "milk", "dessert", "bitter" };
+        public string chocolateType;
+        public Cookie()
+        {
+            chocolateType = _chocolateTypes[new Random().Next(0, 3)];
+        }
+        public void ShowSweet()
+        {
+            Console.WriteLine("This is cookie {0} with {1} chocolate", name, chocolateType);
+        }
+    }
+
+    internal class ChocolateCandy : Candy, IShowSweet
+    {
+        private string[] _chocolateTypes = new string[3] { "milk", "dessert", "bitter" };
+        public string chocolateType;
+        public ChocolateCandy()
+        {
+            chocolateType = _chocolateTypes[new Random().Next(0, 3)];
+        }
+        public void ShowSweet()
+        {
+            Console.WriteLine("This is candy {0} in {1} wrapper with {2} chocolate", name, wrapper, chocolateType);
+        }
+    }
+    internal class MarmaladeCandy : Candy, IShowSweet
+    {
+        private string[] _fruitFlavors = new string[3] { "apple", "juice", "watermelon" };
+        public string flavor;
+        public MarmaladeCandy()
+        {
+            flavor = _fruitFlavors[new Random().Next(0, 3)];
+        }
+        public void ShowSweet()
+        {
+            Console.WriteLine("This is marmalade candy {0} in {1} wrapper with {2} flavor", name, wrapper, flavor);
+        }
+    }
+    internal class ChocolateCandyWithFilling : ChocolateCandy, IShowSweet
+    {
+        private string[] _fillingTypes = new string[4] { "ganache", "truffle", "praline", "nougat" };
+        public string filling;
+        public ChocolateCandyWithFilling()
+        {
+            filling = _fillingTypes[new Random().Next(0, 4)];
+        }
+        public void ShowSweet()
+        {
+            Console.WriteLine("This is candy {0} in {1} wrapper with {2} chocolate and {3} filling", name, wrapper, chocolateType, filling);
+        }
+    }
+
+
+
+
+}
---
+++ NewYearGift/NewYearGift/Services/UserAction.cs
@@ -0,0 +1,82 @@
+﻿using NewYearGift.Models;
+using NewYearGift.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewYearGift.Services
+{
+    internal class UserAction
+    {
+        private GiftBox giftBox = GiftBox.GetInstance;
+        private SortSweets sortSweets = new SortSweets();
+        private FindSweet findSweet = new FindSweet();
+        private Sweet sweet = new Sweet();
+        static void UserCommands()
+        {
+
+            Console.WriteLine("Write \"show box\" - to show your New Year Gift box.");
+            Console.WriteLine("Write \"sort\" - to sort your New Year Gift box by mass.");
+            Console.WriteLine("Write \"show sorted\" - to show sorted by mass New Year Gift box.");
+            Console.WriteLine("Write \"find\" - if you want to find some sweet by name.");
+            Console.WriteLine("Write \"show finded\" - to show finded sweets.");
+            Console.WriteLine("Write \"exit\" - if you want to leave.");
+        }
+
+        public void Start()
+        {
+            UserCommands();
+            giftBox.CreateChildrenGiftBox();
+            while (true)
+            {
+
+                Console.Write("Enter a command: ");
+                string userChoiseVariable = Console.ReadLine().ToLower();
+                switch (userChoiseVariable)
+                {
+                    case "show box":
+                        giftBox.ShowGiftBox();
+                        break;
+                    case "sort":
+                        sortSweets.SortSweetsByMass();
+                        break;
+                    case "show sorted":
+                        sortSweets.ShowSortedBox();
+                        break;
+                    case "show finded":
+                        findSweet.ShowFindedSweets();
+                        break;
+                    case "find":
+                        for (int i = 0; i < sweet.names.Length; i++)
+                        {
+                            Console.WriteLine("Possible name: {0}", sweet.names[i]);
+                        }
+                        Console.Write("Please, write sweet name that you want to find:");
+                        string userChoice = Console.ReadLine();
+                        while (!sweet.names.Contains(userChoice))
+                        {
+                            Console.WriteLine("Wrong sweet name!");
+                            Console.Write("Please, write sweet name, that you want to find:");
+                            userChoice = Console.ReadLine();
+                        }
+                        findSweet.FindSweetsByName(userChoice);
+                        break;
+                    case "exit":
+                        Console.Clear();
+                        Console.WriteLine("Press <Enter> only to exit");
+                        Console.ReadKey();
+                        return;
+                    default:
+                        Console.WriteLine("Wrong command!");
+                        break;
+                }
+
+            }
+
+
+        }
+    }
+}
